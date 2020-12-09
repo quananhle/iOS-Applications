aecd045d5175e5e582eed98600b43792077aa2f5
Quan Le <qle2@hawk.iit.edu>
2020-12-09 17:51:52 -0600

    no message

--- Tip/Tip/ViewController.swift
+++ Tip/Tip/ViewController.swift
@@ -17,12 +17,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
     @IBAction func onTap(_ sender: Any) {
     }
     @IBAction func calculateTip(_ sender: Any) {
+        //get initial bill amount and calculate tips
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        //calculate tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //update the tip and total labels
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
-    
 }
 
