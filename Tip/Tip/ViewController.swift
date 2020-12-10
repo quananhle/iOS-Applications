ca9079fa0ee241a6c711484676b23cca0c3e2e97
Quan Le <qle2@hawk.iit.edu>
2020-12-09 18:07:15 -0600

    no message

--- Tip/Tip/ViewController.swift
+++ Tip/Tip/ViewController.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
     }
-
     @IBAction func onTap(_ sender: Any) {
     }
     @IBAction func calculateTip(_ sender: Any) {
