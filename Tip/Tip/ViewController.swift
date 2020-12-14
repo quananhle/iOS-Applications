39da05d44b100947e8098ab8392d69dee54ea1be
Quan Le <qle2@hawk.iit.edu>
2020-12-14 17:49:14 -0600

    no message

--- Tip/Tip/ViewController.swift
+++ Tip/Tip/ViewController.swift
@@ -19,8 +19,9 @@ class ViewController: UIViewController{
     @IBOutlet weak var tipCustomSlider: UISlider!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var paidButton: UIButton!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -113,6 +114,35 @@ class ViewController: UIViewController{
         else {
             partyNumberTextField.backgroundColor = UIColor.lightGray
             partyNumberTextField.isUserInteractionEnabled = false
+        }
+    }
+}
+
+@IBDesignable extension UIButton {
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
         }
     }
 }
