cee6ad74edc9cbc0c1630b408af400dfc2e9e607
Quan Le <qle2@hawk.iit.edu>
2020-12-17 17:46:55 -0600

    no message

---
+++ Tip/Tip/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  Tip
+//
+//  Created by Ashley Le on 12/17/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
--- Tip/Tip/ViewController.swift
+++ Tip/Tip/ViewController.swift
@@ -50,6 +50,10 @@ class ViewController: UIViewController{
         undoButton.isUserInteractionEnabled = false
         undoButton.setTitleColor(UIColor.lightGray, for: UIControl.State.normal)
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
