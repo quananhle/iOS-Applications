f6750725d53cdff1a5f463a954ce107c6d39f4b2
Quan Le <qle2@hawk.iit.edu>
2020-12-17 23:33:50 -0600

    no message

--- Tip/Tip/SettingsViewController.swift
+++ Tip/Tip/SettingsViewController.swift
@@ -13,7 +13,6 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.title = "Tip Calculator"
     }
     
 
