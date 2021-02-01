b47ffb43728c7c9da82c35aaa177481fc1fc2bd7
Quan Le <qle2@hawk.iit.edu>
2021-01-31 21:44:18 -0600

    no message

---
+++ flixster/flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Ashley Le on 1/31/21.
+//  Copyright © 2021 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
