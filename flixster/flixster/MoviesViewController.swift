010c9231523e7b04ac8d96ac32dfb332ad93bdcb
Quan Le <qle2@hawk.iit.edu>
2021-01-31 22:06:31 -0600

    no message

---
+++ flixster/flixster/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Ashley Le on 1/31/21.
+//  Copyright © 2021 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
