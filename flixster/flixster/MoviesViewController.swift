dd30267cee34d4588f15934c8a23b850d544d54f
Quan Le <qle2@hawk.iit.edu>
2021-02-01 00:06:12 -0600

    no message

--- flixster/flixster/MoviesViewController.swift
+++ flixster/flixster/MoviesViewController.swift
@@ -41,9 +41,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
-        let movie = movies[indexPath.row]
+        let movie = movies["indexPath.row"]
         let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
         cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
         return cell
     }
     /*
