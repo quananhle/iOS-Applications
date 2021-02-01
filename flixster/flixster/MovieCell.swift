12e284db7a6267ef57dd5422b3fcae4a9da41927
Quan Le <qle2@hawk.iit.edu>
2021-01-31 23:45:15 -0600

    no message

---
+++ flixster/flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Ashley Le on 1/31/21.
+//  Copyright © 2021 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
