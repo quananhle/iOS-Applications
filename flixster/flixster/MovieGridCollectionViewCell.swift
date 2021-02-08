449b3ce4c10ccb2f9184be8d9405e9b2c987ed88
Quan Le <qle2@hawk.iit.edu>
2021-02-08 16:16:50 -0500

    no message

---
+++ flixster/flixster/MovieGridCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCollectionViewCell.swift
+//  flixster
+//
+//  Created by Ashley Le on 2/8/21.
+//  Copyright © 2021 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCollectionViewCell: UICollectionViewCell {
+    
+}
