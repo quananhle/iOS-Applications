9aed809cc547151d7ed1694fb0ee2fe942749267
Quan Le <qle2@hawk.iit.edu>
2021-02-08 15:18:51 -0500

    no message

--- flixster/flixster/MovieDetailsViewController.swift
+++ flixster/flixster/MovieDetailsViewController.swift
@@ -30,7 +30,7 @@ class MovieDetailsViewController: UIViewController {
         posterView.af.setImage(withURL: posterUrl!)
         
         let backdropPath = movie["backdrop_path"] as! String
-        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w700" + backdropPath)
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
         backdropView.af.setImage(withURL: backdropUrl!)
     }
     
