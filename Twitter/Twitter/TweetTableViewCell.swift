4e1424597fb4e21eac0996602e54a2fed79a3567
Quan Le <qle2@hawk.iit.edu>
2021-02-16 01:05:57 -0500

    no message

---
+++ Twitter/Twitter/TweetTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Quan Le on 2021/16/2.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
