77f9de3c30c59b57d2d96a5a472346a00f1ec3d1
Quan Le <qle2@hawk.iit.edu>
2021-02-22 23:40:35 -0500

    no message

--- Twitter/Twitter/TweetViewController.swift
+++ Twitter/Twitter/TweetViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class TweetViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func cancel(_ sender: Any) {
+    }
+    @IBAction func tweet(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
