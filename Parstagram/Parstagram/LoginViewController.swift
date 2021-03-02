d6bcde685eda7cb147d94f057ed9d2cb1edd8319
Quan Le <qle2@hawk.iit.edu>
2021-03-02 00:20:34 -0500

    no message

--- Parstagram/Parstagram/LoginViewController.swift
+++ Parstagram/Parstagram/LoginViewController.swift
@@ -12,7 +12,6 @@ import Parse
 class LoginViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
