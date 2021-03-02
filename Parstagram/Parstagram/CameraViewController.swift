6e958b8ae5789dbb693f639c55033d5623f5e222
Quan Le <qle2@hawk.iit.edu>
2021-03-02 01:12:53 -0500

    no message

--- Parstagram/Parstagram/CameraViewController.swift
+++ Parstagram/Parstagram/CameraViewController.swift
@@ -8,33 +8,15 @@
 
 import UIKit
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var commentField: UITextField!
-    
+class CameraViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func onSubmitButton(_ sender: Any) {
-    }
-    @IBAction func onCameraButton(_ sender: Any) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = true
 
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            picker.sourceType = .camera
-        }
-        else {
-            picker.sourceType = .photoLibrary
-        }
-
-        present(picker, animated: true, completion: nil)
-    }
-    
     /*
     // MARK: - Navigation
 
