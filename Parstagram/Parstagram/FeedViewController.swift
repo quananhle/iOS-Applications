c619ccadc5998c7925b9e8a13eedbd68e5ca1c90
Quan Le <qle2@hawk.iit.edu>
2021-03-02 02:37:51 -0500

    no message

--- Parstagram/Parstagram/FeedViewController.swift
+++ Parstagram/Parstagram/FeedViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
     /*
     // MARK: - Navigation
