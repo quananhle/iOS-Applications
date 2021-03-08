7b39b351f997ef79de1061527932c07f21901619
Quan Le <qle2@hawk.iit.edu>
2021-03-08 15:13:34 -0500

    no message

--- Parstagram/Parstagram/SceneDelegate.swift
+++ Parstagram/Parstagram/SceneDelegate.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        if PFUser.current() != nil {
+            let main = UIStoryboard(name: "Main", bundle: nil)
+            self.window = UIWindow(windowScene: windowScene)
+            self.window?.rootViewController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            print("window %@", window)
+            self.window?.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -54,10 +61,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     //            configuration.server = "https://whispering-stream-51370.herokuapp.com/parse"
     //            })
     //        )
-    //        if PFUser.current() != nil {
-    //            let main = UIStoryboard(name: "Main", bundle: nil)
-    //            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
-    //            window?.rootViewController = feedNavigationController
-    //        }
+
 }
 
