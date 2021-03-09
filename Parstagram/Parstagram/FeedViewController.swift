ddb231d7e4225a7bb4a4b317e959fe7f1fc122eb
Quan Le <qle2@hawk.iit.edu>
2021-03-08 23:36:42 -0500

    no message

--- Parstagram/Parstagram/FeedViewController.swift
+++ Parstagram/Parstagram/FeedViewController.swift
@@ -24,6 +24,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         let query = PFQuery(className:"Posts")
         query.includeKey("author")
+//        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
