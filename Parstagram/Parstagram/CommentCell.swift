4b7f31b12e02da293aff901d1aa26f4c5f806047
Quan Le <qle2@hawk.iit.edu>
2021-03-08 21:40:21 -0500

    no message

---
+++ Parstagram/Parstagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Ashley Le on 3/8/21.
+//  Copyright © 2021 codepath. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
